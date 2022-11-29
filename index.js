bc9b0aa39d456d1e9550ddcad9e02a69d6924b63
amanda vikdal <vikdal.amanda@gmail.com>
2022-11-28 18:27:48 -0700

    commit tree 2032903658754ba9311e6fb2a3a5b4952ca0a291

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", () => console.log("the DOM has loaded"));
+
+// const textId = document.getElementById("text")
+
+// function changeText (){
+//     textId.textContent= "This is really cool"
+// }
+// changeText(textId);
+
+const textId = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", function changeText (){
+    textId.textContent= "This is really cool!"
+}
+// changeText(textId);
+);
+
+// const textId = document.getElementById("text")
+
+// function changeText (){
+//     textId.textContent= "This is really cool"
+// }
+// changeText(textId);
+
+console.log("The DOM is loading");
+
+
